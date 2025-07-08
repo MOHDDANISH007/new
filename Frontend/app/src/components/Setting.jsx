9470061f323aed4c0dd4c063892ae090f4c268b7
Mohammad Danish <m.danish00707@gmail.com>
2025-07-08 15:10:06 +0530

    Added

--- Frontend/app/src/components/Setting.jsx
+++ Frontend/app/src/components/Setting.jsx
@@ -114,9 +114,11 @@ const Setting = () => {
             We couldn't find any financial data associated with your account.
             Please add your financial information to get started.
           </p>
-          <button className='bg-indigo-600 text-white px-6 py-2 rounded-lg hover:bg-indigo-700 transition'>
-            Add Financial Data
-          </button>
+          <Link to='/form'>
+            <button className='bg-indigo-600 text-white px-6 py-2 rounded-lg hover:bg-indigo-700 transition'>
+              Add Financial Data
+            </button>
+          </Link>
         </div>
       </div>
     )
