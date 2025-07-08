51933a4ae5bcc448556fa679aea9bdf0478d00fd
Mohammad Danish <m.danish00707@gmail.com>
2025-07-08 17:32:59 +0530

    Added

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -7,23 +7,15 @@ import WelcomeSection from './components/Welcome'
 
 const App = () => {
   const { user } = useAuth()
-
   return (
-    <div className='min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex'>
+    <div className='min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100'>
       <SiderBar isAuthenticated={!!user} />
-
-      <div className={`flex-1 ${user ? 'lg:ml-64' : ''}`}>
-        {user ? (
-          // Main content if user is logged in
-          <div className='min-h-screen flex items-center justify-center p-4'>
-            <Outlet />
-          </div>
-        ) : (
-          // Show WelcomeSection if no user
-          <div className='min-h-screen flex items-center justify-center'>
-            <WelcomeSection />
-          </div>
-        )}
+      {!user && <WelcomeSection />}
+      {/* Main Content Area */}
+      <div className={user ? 'lg:ml-64' : ''}>
+        <div className='min-h-screen flex items-center justify-center p-4'>
+          <Outlet />
+        </div>
       </div>
     </div>
   )
