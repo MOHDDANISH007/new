fe3156151c79e5c6f9934cbec61cc6396064f66b
Mohammad Danish <m.danish00707@gmail.com>
2025-07-08 17:54:08 +0530

    Added

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -1,36 +1,26 @@
 // App.jsx
-import React, { useEffect } from 'react'
+import React from 'react'
 import SiderBar from './components/SiderBar'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { useAuth } from './context/Auth.context'
 import WelcomeSection from './components/Welcome'
 
 const App = () => {
   const { user } = useAuth()
-  const navigate = useNavigate()
-  const location = useLocation()
-
-  // Redirect to "/" after login, but NOT if already on another route like "/document"
-  useEffect(() => {
-    if (!user) {
-      navigate('/login')
-    }
-  }, [user,navigate])
-
+  const [count, setCount] = React.useState(0)
   return (
-    <div className='min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex'>
+    <div className='min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100'>
       <SiderBar isAuthenticated={!!user} />
-
-      <div className={`flex-1 ${user ? 'lg:ml-64' : ''}`}>
-        {user ? (
-          <div className='min-h-screen flex items-center justify-center p-4'>
-            <Outlet />
-          </div>
-        ) : (
-          <div className='min-h-screen flex items-center justify-center'>
-            <WelcomeSection />
-          </div>
-        )}
+      {count === 0 && (
+        <div>
+          <WelcomeSection count={count} setCount={setCount} />
+        </div>
+      )}
+      {/* Main Content Area */}
+      <div className={user ? 'lg:ml-64' : ''}>
+        <div className='min-h-screen flex items-center justify-center p-4'>
+          <Outlet />
+        </div>
       </div>
     </div>
   )
